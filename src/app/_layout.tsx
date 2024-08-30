5b653207b24a65d049c2aa58c7ad5c25e944279d
0darkace1 <0darkace1@gmail.com>
2024-08-30 18:33:59 +0200

    Initial Commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,20 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
